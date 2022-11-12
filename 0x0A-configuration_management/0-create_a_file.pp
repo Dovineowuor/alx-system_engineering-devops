3a4858d8667a70f88e9f403304f6b838370069dd
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-11-12 07:12:42 -0800

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Task 0 - Create a file in temp Using Puppet
+file { '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
