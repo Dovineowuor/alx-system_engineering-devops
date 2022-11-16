463b2b6ca02dd2844e161579bdbca08842a3c60d
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-11-16 15:33:27 +0300

    debug

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-'''
+"""
 A script to export data in the JSON format.
-'''
+#!/usr/bin/python3
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+if __name__ == "__main__":
 
-import json
-import requests
-from sys import argv
+    import requests
+    import sys
+    import json
 
-if __name__ == '__main__':
-    uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
-    todo = requests.get(url, verify=False).json()
-    name = user.get('username')
-    t = [{"task": t.get("title"),
-          "username": name,
-          "completed": t.get("completed")} for t in todo]
-    bj = {}
-    bj[uid] = t
-    with open("{}.json".format(uid), 'w') as filejs:
-        json.dump(bj, filejs)
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        u_url = 'https://jsonplaceholder.typicode.com/users/'
+        td_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+
+        USER_ID = requests.get(u_url + sys.argv[1]).json()['id']
+        USERNAME = requests.get(u_url + sys.argv[1]).json()['username']
+        TASK_COMPLETED_STATUSES = [task['completed'] for task in
+                                   requests.get(td_url + sys.argv[1]).json()]
+        TASKS_TITLES = [task['title'] for task in
+                        requests.get(td_url + sys.argv[1]).json()]
+        TOTAL_NUMBER_OF_TASKS = len(requests.get(td_url + sys.argv[1]).json())
+
+        TASKS_DICT = {"{}".format(USER_ID):
+                      [{"task": "{}".format(TASKS_TITLES[N]),
+                        "completed": TASK_COMPLETED_STATUSES[N],
+                        "username": "{}".format(USERNAME)}
+                       for N in range(TOTAL_NUMBER_OF_TASKS)]}
+
+        with open('{}.json'.format(USER_ID), 'w') as fp:
+            json.dump(TASKS_DICT, fp)
