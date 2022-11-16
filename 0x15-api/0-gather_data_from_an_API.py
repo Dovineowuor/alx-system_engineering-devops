afd5c2b6f5de9f3224adcb308b05672aa2ec1af6
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-11-16 16:17:45 +0300

    pep 8 compilation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
                   .format(name, count, alltsk))
             for title in completedtsk:
                 print("\t {}".format(title.get("title")))
+                
